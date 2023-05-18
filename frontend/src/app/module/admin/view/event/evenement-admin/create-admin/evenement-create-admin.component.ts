e4bde7d6ad6cd233a9235e8e30da7e90d36a98a2
younes.zouani <y.zouani@uca.ma>
2023-05-18 21:46:42 +0100

    algo redis

--- frontend/src/app/module/admin/view/event/evenement-admin/create-admin/evenement-create-admin.component.ts
+++ frontend/src/app/module/admin/view/event/evenement-admin/create-admin/evenement-create-admin.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { AbstractCreateController } from 'src/app/zynerator/controller/AbstractCreateController';
+import {AbstractCreateController} from 'src/app/zynerator/controller/AbstractCreateController';
 
 import {EvenementService} from 'src/app/controller/service/Evenement.service';
 import {EvenementDto} from 'src/app/controller/model/Evenement.model';
@@ -9,98 +9,104 @@ import {EvenementStateDto} from 'src/app/controller/model/EvenementState.model';
 import {EvenementStateService} from 'src/app/controller/service/EvenementState.service';
 import {SalleDto} from 'src/app/controller/model/Salle.model';
 import {SalleService} from 'src/app/controller/service/Salle.service';
+
 @Component({
-  selector: 'app-evenement-create-admin',
-  templateUrl: './evenement-create-admin.component.html'
+    selector: 'app-evenement-create-admin',
+    templateUrl: './evenement-create-admin.component.html'
 })
-export class EvenementCreateAdminComponent extends AbstractCreateController<EvenementDto, EvenementCriteria, EvenementService>  implements OnInit {
+export class EvenementCreateAdminComponent extends AbstractCreateController<EvenementDto, EvenementCriteria, EvenementService> implements OnInit {
 
 
-
-   private _validEvenementReference = true;
+    private _validEvenementReference = true;
     private _validSalleReference = true;
     private _validSalleCode = true;
     private _validEvenementStateReference = true;
     private _validEvenementStateCode = true;
 
-    constructor( private evenementService: EvenementService , private evenementStateService: EvenementStateService, private salleService: SalleService) {
+    constructor(private evenementService: EvenementService, private evenementStateService: EvenementStateService, private salleService: SalleService) {
         super(evenementService);
     }
 
     ngOnInit(): void {
-    this.salle = new SalleDto();
-    this.salleService.findAll().subscribe((data) => this.salles = data);
-    this.evenementState = new EvenementStateDto();
-    this.evenementStateService.findAll().subscribe((data) => this.evenementStates = data);
-}
+        this.salle = new SalleDto();
+        this.salleService.findAll().subscribe((data) => this.salles = data);
+        this.evenementState = new EvenementStateDto();
+        this.evenementStateService.findAll().subscribe((data) => this.evenementStates = data);
+    }
 
 
- save() {
+    save() {
+        super.save();
+    }
 
 
-     super.save();
- }
-
-
-    public setValidation(value: boolean){
+    public setValidation(value: boolean) {
         this.validEvenementReference = value;
     }
 
 
-
-    public validateForm(): void{
+    public validateForm(): void {
         this.errorMessages = new Array<string>();
         this.validateEvenementReference();
     }
 
-    public validateEvenementReference(){
+    public validateEvenementReference() {
         if (this.stringUtilService.isEmpty(this.item.reference)) {
-        this.errorMessages.push('Reference non valide');
-        this.validEvenementReference = false;
+            this.errorMessages.push('Reference non valide');
+            this.validEvenementReference = false;
         } else {
             this.validEvenementReference = true;
         }
     }
 
 
-
     get salle(): SalleDto {
         return this.salleService.item;
     }
+
     set salle(value: SalleDto) {
         this.salleService.item = value;
     }
+
     get salles(): Array<SalleDto> {
         return this.salleService.items;
     }
+
     set salles(value: Array<SalleDto>) {
         this.salleService.items = value;
     }
+
     get createSalleDialog(): boolean {
-       return this.salleService.createDialog;
+        return this.salleService.createDialog;
     }
+
     set createSalleDialog(value: boolean) {
-        this.salleService.createDialog= value;
+        this.salleService.createDialog = value;
     }
+
     get evenementState(): EvenementStateDto {
         return this.evenementStateService.item;
     }
+
     set evenementState(value: EvenementStateDto) {
         this.evenementStateService.item = value;
     }
+
     get evenementStates(): Array<EvenementStateDto> {
         return this.evenementStateService.items;
     }
+
     set evenementStates(value: Array<EvenementStateDto>) {
         this.evenementStateService.items = value;
     }
+
     get createEvenementStateDialog(): boolean {
-       return this.evenementStateService.createDialog;
-    }
-    set createEvenementStateDialog(value: boolean) {
-        this.evenementStateService.createDialog= value;
+        return this.evenementStateService.createDialog;
     }
 
+    set createEvenementStateDialog(value: boolean) {
+        this.evenementStateService.createDialog = value;
+    }
 
 
     get validEvenementReference(): boolean {
@@ -108,30 +114,37 @@ export class EvenementCreateAdminComponent extends AbstractCreateController<Even
     }
 
     set validEvenementReference(value: boolean) {
-         this._validEvenementReference = value;
+        this._validEvenementReference = value;
     }
 
     get validSalleReference(): boolean {
         return this._validSalleReference;
     }
+
     set validSalleReference(value: boolean) {
         this._validSalleReference = value;
     }
+
     get validSalleCode(): boolean {
         return this._validSalleCode;
     }
+
     set validSalleCode(value: boolean) {
         this._validSalleCode = value;
     }
+
     get validEvenementStateReference(): boolean {
         return this._validEvenementStateReference;
     }
+
     set validEvenementStateReference(value: boolean) {
         this._validEvenementStateReference = value;
     }
+
     get validEvenementStateCode(): boolean {
         return this._validEvenementStateCode;
     }
+
     set validEvenementStateCode(value: boolean) {
         this._validEvenementStateCode = value;
     }
